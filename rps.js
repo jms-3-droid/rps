075d40e604ce9fd7c0278799fe09a0c6f76322e4
JMS <180058073+jms-3-droid@users.noreply.github.com>
2025-06-09 14:09:58 -0700

    code rps

--- rps.js
+++ rps.js
@@ -1,14 +1,58 @@
 console.log("Hello World");
 
-function getComputerChoice() {
+function playGame() {
 
-  const randomNum = Math.random();
+  let humanScore = 0;
+  let computerScore = 0;
 
-  if (randomNum <= .33) {
-    return "rock";
-  } else if (randomNum <= .66) {
-    return "paper";
-  } else {
-    return "scissors";
+  function getComputerChoice() {
+
+    const randomNum = Math.random();
+
+    if (randomNum <= .33) {
+      return "rock";
+    } else if (randomNum <= .66) {
+      return "paper";
+    } else {
+      return "scissors";
+      }
+    }
+
+  function getHumanChoice() {
+    let choice = window.prompt("rock paper or scissors?", "shoot");
+    let humanChoice = choice.toLowerCase();
+    return humanChoice;
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("Draw!");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log("You Win!");
+        humanScore++;
+    } else {
+      console.log("You Lose!");
+      computerScore++;
+      }
+    }
+
+  for (let round = 1; round <= 5; round++) {
+    console.log("Round ${round}");
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
+
+  if (humanScore === computerScore) {
+    console.log("Tie Game!");
+    } else if (humanScore > computerScore) {
+      console.log("You Win!");
+    } else {
+      console.log("You Lose!");
     }
   }
