21e53f30336f8d14ab6f03dcdd6132fa757e8fe1
JMS <180058073+jms-3-droid@users.noreply.github.com>
2025-06-09 12:52:04 -0700

    code computer choice

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 Create a Rock Paper Scissors game
 Played entirely in console
+Played against the computer
+Write a function that randomly returns "rock", "paper", or "scissors"
--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+
+  const randomNum = Math.random();
+
+  if (randomNum <= .33) {
+    return "rock";
+  } else if (randomNum <= .66) {
+    return "paper";
+  } else {
+    return "scissors";
+    }
+  }
