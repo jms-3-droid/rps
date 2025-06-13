f8cc9ca1f066f9ad14a29f3204b0e6dda9e4e38e
JMS <180058073+jms-3-droid@users.noreply.github.com>
2025-06-13 14:08:55 -0700

    code ui

--- rps.js
+++ rps.js
@@ -1,51 +1,51 @@
 function getComputerChoice () {
-   let randomNumber = Math.floor(Math.random()*3);
-   const gameVariants = ['Rock', 'Paper', 'Scissors'];  
-   return (gameVariants[randomNumber])
+  let randomNumber = Math.floor(Math.random()*3);
+  const gameVariants = ['Rock', 'Paper', 'Scissors'];  
+  return (gameVariants[randomNumber])
      
 }
 
 function getHumanChoice() {
-    let humanVariant = prompt('Rock, Paper, Scissors');
-    return humanVariant;
+  let humanVariant = prompt('Rock, Paper, Scissors');
+  return humanVariant;
 }
 
 function playRound (humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-    if (humanChoice === computerChoice) {
-        console.log("Draw");
-    } else if (
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-        console.log('You win!'); 
-        return 'human'; 
-       }
-       else {
-        console.log('Computer wins!');
-        return 'computer';
-       }
-    }
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("Draw");
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+    console.log('You win!'); 
+    return 'human'; 
+    } else {
+      console.log('Computer wins!');
+      return 'computer';
+      }
+}
  
-    function playgame () {
-        let humanScore = 0;
-        let computerScore = 0;
+function playgame () {
+  let humanScore = 0;
+  let computerScore = 0;
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  const result = playRound (humanSelection, computerSelection);
 
-        for ( let i = 1; i <=5; i++ ) {
-            const humanSelection = getHumanChoice();
-            const computerSelection = getComputerChoice();
-            
-            const result = playRound (humanSelection, computerSelection);
+  const rockBtn = document.createElement("button");
+  const paperBtn = document.createElement("button");
+  const scissorsBtn = document.createElement("button");
+  
+  const display = document.createElement("div");
+  display.textContent = `Results: Human ${humanScore} Computer ${computerScore}`;
 
-            if (result === 'human') {
-                humanScore++;
-            }
-            else if  (result === 'computer'){
-                computerScore++;
-            }
-        }
-        console.log(`Results: Human ${humanScore} Computer ${computerScore}`);
-    }
+  if (result === 'human') {
+    humanScore++;
+  } else if  (result === 'computer'){
+    computerScore++;
+  }
+}
 playgame();
